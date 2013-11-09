b408acf4089f8f40a9589254835be3acea2445de
Erick Daniel Corona <edcoronag@gmail.com>
2013-11-09 16:50:21 -0600

    changed return types and token/lexeme checker functions

--- code/headers/Parser.hpp
+++ code/headers/Parser.hpp
@@ -16,49 +16,50 @@ public:
   Parser(FileReader* fileReader, ErrorReporter* errorReporter);
   Parser(const Parser& source);
 
-  int parse();
+  void parse();
 
   Parser& operator=(const Parser& rhs);
 private:
-  int andoperator(bool isLookedForward);
-  int assign(bool isLookedForward);
-  int block(bool isLookedForward);
-  int caseStatement(bool isLookedForward);
-  int command(bool isLookedForward);
-  int constantDeclaration(bool isLookedForward);
-  int dimension(bool isLookedForward);
-  int exponent(bool isLookedForward);
-  int expression(bool isLookedForward);
-  int forStatement(bool isLookedForward);
-  int functionDeclaration(bool isLookedForward);
-  int functionSign(bool isLookedForward);
-  int ifStatement(bool isLookedForward);
-  int import(bool isLookedForward);
-  int multiplication(bool isLookedForward);
-  int notOperator(bool isLookedForward);
-  int parameterList(bool isLookedForward);
-  int parameters(bool isLookedForward);
-  int print(bool isLookedForward);
-  int program();
-  int useDimenison(bool isLookedForward);
-  int read(bool isLookedForward);
-  int relational(bool isLookedForward);
-  int returnExpression(bool isLookedForward);
-  int returnType(bool isLookedForward);
-  int sign(bool isLookedForward);
-  int statement(bool isLookedForward);
-  int term(bool isLookedForward);
-  int type(bool isLookedForward);
-  int useParameters(bool isLookedForward);
-  int variables(bool isLookedForward);
+  void andoperator();
+  void assign();
+  void block();
+  void caseStatement();
+  void command();
+  void constantDeclaration();
+  void dimension();
+  void exponent();
+  void expression();
+  void forStatement();
+  void functionDeclaration();
+  void functionSign();
+  void ifStatement();
+  void import();
+  void multiplication();
+  void notOperator();
+  void parameterList();
+  void parameters();
+  void print();
+  void program();
+  void useDimenison();
+  void read();
+  void relational();
+  void returnExpression();
+  void returnType();
+  void sign();
+  void statement();
+  void term();
+  void type();
+  void useParameters();
+  void variables();
 
-  bool isLexemeCorrect(const std::string& lexeme);
-  bool isTokenCorrect(TokenType_t token);
+  void checkToken(TokenType_t token);
+  void checkLexeme(const std::string& lexeme);
 
   int            m_tokenNo;
   TokenLexeme    m_currentToken;
   Scanner        m_scanner;
   ErrorReporter* m_errorReporter;
+  int            m_maxErrors;
 };
 
 #endif /* PARSER_HPP */
--- code/source/Parser.cpp
+++ code/source/Parser.cpp
@@ -1,5 +1,6 @@
 /* Definition of member functions of class Parser */
 
+
 #include "../headers/Parser.hpp"
 
 #include <string>
@@ -14,188 +15,248 @@ Parser::Parser(FileReader* fileReader, ErrorReporter* errorReporter) :
   m_tokenNo(0),
   m_currentToken(),
   m_scanner(Scanner(fileReader, errorReporter)),
-  m_errorReporter(errorReporter)
+  m_errorReporter(errorReporter),
+  m_maxErrors(5)
 {
+  m_maxErrors = m_errorReporter->getMaxErrors();
 }
 
-int Parser::parse() {
+void Parser::parse()
+{
   m_scanner.scan();
 
-  if (m_errorReporter->getErrors() < m_errorReporter->getMaxErrors()) {
-    if (program() == EXIT_FAILURE)
-      return EXIT_FAILURE;                     
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+
+  program();
 
 #ifdef DEBUG
     cout << "expected tokens: " << m_tokenNo << ", got: " <<
         m_scanner.getMaxTokens() << endl;
 #endif
-    if (m_tokenNo != m_scanner.getMaxTokens()) {
-      m_errorReporter->writeError(
-          "codigo incompleto, cantidad de tokens incongruente");
-      return EXIT_FAILURE;
-    }
-  }
-  else
-    return EXIT_FAILURE;
-  
-  return EXIT_SUCCESS;
-}
-
-int Parser::andoperator(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::assign(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::block(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::caseStatement(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::command(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::constantDeclaration(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::dimension(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::exponent(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::expression(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::forStatement(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::functionDeclaration(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::functionSign(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::ifStatement(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::import(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::multiplication(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::notOperator(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::parameterList(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::parameters(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::print(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::program() {
-  if (!isLexemeCorrect("paquete"))
-    return EXIT_FAILURE;
-
-  if (!isLexemeCorrect("principal"))
-    return EXIT_FAILURE;
-
-  m_currentToken = m_scanner.getNextTokenLexeme();
-  ++m_tokenNo;
-  if (m_currentToken.getLexeme().compare("var") == 0)
+  if (m_tokenNo != m_scanner.getMaxTokens())
   {
-    if (variables(true) == EXIT_FAILURE)
-      return EXIT_FAILURE;
+    m_errorReporter->writeError(
+        "codigo incompleto, cantidad de tokens incongruente");
   }
-  else if (m_currentToken.getLexeme().compare("const") == 0)
-  {
-    if (constantDeclaration(true) == EXIT_FAILURE)
-      return EXIT_FAILURE;
-  }
-  else if (m_currentToken.getLexeme().compare("funcion") == 0)
-  {
-    if (functionDeclaration(true) == EXIT_FAILURE)
-      return EXIT_FAILURE;
-  }
-  else
-  {
-    return EXIT_FAILURE;
-  }
-
-  return EXIT_SUCCESS;
 }
 
-int Parser::useDimenison(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::read(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::relational(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::returnExpression(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::returnType(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::sign(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::statement(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::term(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::type(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::useParameters(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-int Parser::variables(bool isLookedForward) {
-  return EXIT_SUCCESS;
-}
-
-bool Parser::isTokenCorrect(TokenType_t expectedToken)
+void Parser::andoperator()
 {
-  if (m_errorReporter->getErrors() < m_errorReporter->getMaxErrors())
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::assign()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::block()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::caseStatement()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::command()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::constantDeclaration()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::dimension()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::exponent()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::expression()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::forStatement()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::functionDeclaration()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::functionSign()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::ifStatement()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::import()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::multiplication()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::notOperator()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::parameterList()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::parameters()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::print()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::program()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+
+  checkLexeme("paquete");
+
+  checkLexeme("principal");
+
+  do
+  {
+    m_currentToken = m_scanner.getNextTokenLexeme();
+    ++m_tokenNo;
+    if (m_currentToken.getLexeme().compare("var") == 0)
+    {
+      variables();
+    }
+    else if (m_currentToken.getLexeme().compare("const") == 0)
+    {
+      constantDeclaration();
+    }
+    else if (m_currentToken.getLexeme().compare("funcion") == 0)
+    {
+      functionDeclaration();
+    }
+  } while ((m_currentToken.getLexeme().compare("var")   ||
+            m_currentToken.getLexeme().compare("const") ||
+            m_currentToken.getLexeme().compare("funcion")) &&
+            m_tokenNo < m_scanner.getMaxTokens());
+}
+
+void Parser::useDimenison()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::read()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::relational()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::returnExpression()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::returnType()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::sign()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::statement()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::term()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::type()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::useParameters()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+}
+
+void Parser::variables()
+{
+  if (m_errorReporter->getErrors() >= m_maxErrors)
+    return;
+
+  checkToken(TOKEN_IDEN);
+}
+
+void Parser::checkToken(TokenType_t expectedToken)
+{
+  if (m_errorReporter->getErrors() < m_maxErrors)
   {
     m_currentToken = m_scanner.getNextTokenLexeme();
     ++m_tokenNo;
@@ -204,20 +265,13 @@ bool Parser::isTokenCorrect(TokenType_t expectedToken)
       m_errorReporter->writeSyntaxError(expectedToken,
           m_currentToken.getToken(), m_currentToken.getLine(),
           m_currentToken.getRow());
-      return false;
     }
   }
-  else
-  {
-    return false;
-  }
-
-  return true;
 }
 
-bool Parser::isLexemeCorrect(const string& expectedLexeme)
+void Parser::checkLexeme(const string& expectedLexeme)
 {
-  if (m_errorReporter->getErrors() < m_errorReporter->getMaxErrors())
+  if (m_errorReporter->getErrors() < m_maxErrors)
   {
     m_currentToken = m_scanner.getNextTokenLexeme();
     ++m_tokenNo;
@@ -226,14 +280,7 @@ bool Parser::isLexemeCorrect(const string& expectedLexeme)
       m_errorReporter->writeSyntaxError(expectedLexeme,
           m_currentToken.getLexeme(), m_currentToken.getLine(),
           m_currentToken.getRow());
-      return false;
     }
   }
-  else
-  {
-    return false;
-  }
-
-  return true;
 }
 
