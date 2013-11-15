f8b1168fbf8cf5b154310c5f824cd6cad772462f
Erick Daniel Corona <edcoronag@gmail.com>
2013-11-15 10:23:12 -0600

    added more functions

--- resources/class/Function.java
+++ resources/class/Function.java
@@ -5,8 +5,20 @@ public class Function
     return 124;
   }
 
+  static public void sayHello()
+  {
+    System.out.println("Hello");
+  }
+
+  static protected String giveAnswer()
+  {
+    return "AnAnswer";
+  }
+
   public static void main(String[] argv)
   {
+    System.out.println(giveAnswer());
     System.out.println(function());
+    sayHello();
   }
 }
