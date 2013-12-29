75df49d6811c370a30cb18f2eb8d0d6ef924f051
Erick Daniel Corona <edcoronag@gmail.com>
2013-12-29 09:18:14 -0600

    splitted build scripts in debug/release modes

--- testfiles/build_run.sh
+++ testfiles/build_run.sh
@@ -4,31 +4,14 @@
 
 clear
 
-flags='-Wall -Wextra -Wc++11-compat -Wswitch -Wshadow -Weffc++ -g -std=c++11 -D DEBUG'
-
-binary_dir='../binaries'
-source_dir='../code/source'
-
-source_files="$source_dir/main.cpp
-              $source_dir/Parser.cpp
-              $source_dir/Scanner.cpp
-              $source_dir/FileReader.cpp
-              $source_dir/ErrorReporter.cpp
-              $source_dir/TokenLexeme.cpp"
-
-
-if [ ! -d $binary_dir ]; then
-  mkdir $binary_dir
-fi
-
-g++ $flags -o $binary_dir/vecomp $source_files
+./build_debug.sh
 
 if [ $? -eq 0 ]; then
   if [ -e errores.out ]; then
     rm errores.out
   fi
 
-  $binary_dir/vecomp $1 
+  ../binaries/vecomp $1 
 fi
 
 if [ $? -eq 0 ]; then
